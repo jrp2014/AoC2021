55b9932ae992d87b65e0f450d8ea1807aa71e9e1
jrp2014 <jrp2014@users.noreply.github.com>
2021-12-03 00:29:17 +0000

    Add neater but less obvious solution to Day 1, part 2

--- Day01/Day01.hs
+++ Day01/Day01.hs
@@ -7,7 +7,7 @@ parse :: String -> [Int]
 parse = map read . lines
 
 part1 :: [Int] -> Int
-part1 depths = length . filter (== True) $ zipWith (<) depths (tail depths)
+part1 depths = length . filter (== True) $ zipWith (<) depths (drop 1 depths)
 
 part2 :: [Int] -> Int
 part2 depths = part1 depths'
@@ -19,6 +19,12 @@ part2 depths = part1 depths'
       where
         transpose' :: [[a]] -> [[a]]
         transpose' = getZipList . traverse ZipList
+
+-- A neater solution, observing that, when comparing the sums of the slidng
+-- window, only the first element of the first window, and the last element of
+-- the second, matter
+part2' :: [Int] -> Int
+part2' depths = length . filter (== True) $ zipWith (<) depths (drop 3 depths)
 
 main :: IO ()
 main = do
@@ -37,3 +43,6 @@ main = do
 
   putStr "Part 2: "
   print $ part2 pinput
+
+  putStr "Part 2': "
+  print $ part2' pinput
