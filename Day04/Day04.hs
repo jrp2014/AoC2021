4ea5cece5f26132a69f6b476ca0ec7cd37f919a7
jrp2014 <jrp2014@users.noreply.github.com>
2021-12-04 12:28:11 +0000

    Day 4

---
+++ Day04/Day04.hs
@@ -0,0 +1,63 @@
+module Day04 where
+
+-- https://gitlab.com/sakisan/adventofcode/-/blob/2021/Haskell/Day04.hs
+
+import Data.List (find, inits, partition, sort, transpose)
+import Data.List.Split (splitOn)
+import Data.Maybe (mapMaybe)
+
+type Draws = [Int]
+
+type Row = [Int]
+
+type Card = [Row]
+
+type Input = (Draws, [Card])
+
+isWinningCard :: Card -> Draws -> Bool
+isWinningCard card draws = complete card || complete (transpose card)
+  where
+    complete = any (all (`elem` draws))
+
+score :: Card -> Draws -> Int
+score card draws = last draws * sum unmarked
+  where
+    (_marked, unmarked) = partition (`elem` draws) (concat card)
+
+findWin :: Draws -> Card -> Maybe (Int, Int)
+findWin draws card =
+  (,) <$> length <*> score card
+    <$> find (isWinningCard card) (inits draws)
+
+parse :: String -> Input
+parse s = (map read $ splitOn "," rawDraws, map parseCard $ splitOn [""] rawCards)
+  where
+    (rawDraws : rawCards) = lines s
+    parseCard = map (map read . words)
+
+pickCard :: ([(Int, Int)] -> (Int, Int)) -> Input -> Int
+pickCard f (draws, cards) = snd . f . sort $ mapMaybe (findWin draws) cards
+
+part1 :: Input -> Int
+part1 = pickCard head
+
+part2 :: Input -> Int
+part2 = pickCard last
+
+main :: IO ()
+main = do
+  putStr "Test Part 1: "
+  tinput <- readFile "test.txt"
+  let ptinput = parse tinput
+  print $ part1 ptinput
+
+  putStr "Part 1: "
+  input <- readFile "input.txt"
+  let pinput = parse input
+  print $ part1 pinput
+
+  putStr "Test Part 2: "
+  print $ part2 ptinput
+
+  putStr "Part 2: "
+  print $ part2 pinput
--- README.md
+++ README.md
@@ -6,18 +6,19 @@ Example run:
 
 
 ```
-❯ cd Day01
+❯ cd Day04
 
-❯ ghci -Wall Day01.hs
+❯ ghci -Wall -package split Day04.hs
 GHCi, version 8.10.7: https://www.haskell.org/ghc/  :? for help
+Loaded package environment from /Users/jrp/.ghc/x86_64-darwin-8.10.7/environments/default
 Loaded GHCi configuration from /Users/jrp/.ghci
-[1 of 1] Compiling Day01            ( Day01.hs, interpreted )
+[1 of 1] Compiling Day04            ( Day04.hs, interpreted )
 Ok, one module loaded.
-*Day01> main
-Test Part 1: 7
-Part 1: 1475
-Test Part 2: 5
-Part 2: 1516
-*Day01> :q
+*Day04> main
+Test Part 1: 4512
+Part 1: 2496
+Test Part 2: 1924
+Part 2: 25925
+*Day04> :q
 Leaving GHCi.
 ```
