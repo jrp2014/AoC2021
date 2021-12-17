cd2e388ed33af8854c605fd665c785ee5f46c7a9
jrp2014 <jrp2014@users.noreply.github.com>
2021-12-17 18:57:23 +0000

    Day 17, aternative

--- Day17/Day17.hs
+++ Day17/Day17.hs
@@ -1,11 +1,19 @@
 module Day17.Day17 where
 
+-- https://github.com/glguy/advent2021/blob/main/execs/Day17.hs
+
 import Data.Char (isDigit)
 import Data.List (sort)
 
 type Coord = (Int, Int) -- (y, x)
 
 type TargetArea = (Coord, Coord) -- ((yMin, xMin), (yMax, xMax))
+
+-- | The state of a traveling probe
+data Probe
+  = -- | x-position y-position x-velocity y-velocity
+    P !Int !Int !Int !Int
+  deriving (Show)
 
 parse :: String -> TargetArea
 parse input = ((yMin, xMin), (yMax, xMax))
@@ -17,36 +25,48 @@ parse input = ((yMin, xMin), (yMax, xMax))
     [xMin, xMax] = sort [x1, x2]
     [yMin, yMax] = sort [y1, y2]
 
-ys :: TargetArea -> Int -> [Int]
-ys ((yMin, _), _) y'0 =
-  takeWhile (>= yMin) $
-    scanl (+) 0 $ iterate (\y -> y - 1) y'0
 
-xs :: Int -> [Int]
-xs x'0 = scanl (+) 0 $ iterate (\x -> x - signum x) x'0
+-- | Advance the probe one timestep
+step :: Probe -> Probe
+step (P x y vx vy) = P (x + vx) (y + vy) (vx - signum vx) (vy - 1)
 
-xss :: TargetArea -> [[Int]]
-xss (_, (_, xMax)) = xs <$> [0 .. xMax]
+-- | Run a simulation returning the maximum height seen if
+-- the probe ever succeeds in hitting the target.
+--
+-- >>> sim 20 30 (-10) (-5) 6 9
+-- Just 45
+sim ::
+  -- | target x lo
+  Int ->
+  -- | target x hi
+  Int ->
+  -- | target y lo
+  Int ->
+  -- | target y hi
+  Int ->
+  -- | initial x velocity
+  Int ->
+  -- | initial y velocity
+  Int ->
+  -- | maximum height if successful
+  Maybe Int
+sim xlo xhi ylo yhi vx0 vy0 = go 0 (P 0 0 vx0 vy0)
+  where
+    go best p@(P x y _ _)
+      | y < ylo || x > xhi = Nothing -- too far
+      | xlo <= x, x <= xhi, ylo <= y, y <= yhi = Just best
+      | otherwise = go (max y best) (step p)
 
-yss :: TargetArea -> [[Int]]
-yss ta@((yMin, yMax), _) = [ys' | ys' <- ys ta <$> [yMin .. abs yMin], last ys' <= yMax]
-
-inTargetArea :: TargetArea -> Coord -> Bool
-inTargetArea ((yMin, xMin), (yMax, xMax)) (x, y) =
-  y >= yMin && y <= yMax
-    && x >= xMin
-    && x <= xMax
-
-trajectories :: TargetArea -> [[Coord]]
-trajectories ta =
-  [ trajectory | ys <- yss ta, xs <- xss ta, let trajectory = zip xs ys, any (inTargetArea ta) trajectory
-  ]
+solve :: TargetArea -> [Int]
+solve ((ylo, xlo), (yhi, xhi))
+  | xlo >= 0 && yhi <= 0 =
+      [y | vx <- [0 .. xhi], vy <- [ylo .. -ylo], Just y <- [sim xlo xhi ylo yhi vx vy]]
 
 part1 :: TargetArea -> Int
-part1 ta = maximum . map snd $ last (trajectories ta)
+part1 ta = maximum (solve ta)
 
 part2 :: TargetArea -> Int
-part2 ta = length $ trajectories ta
+part2 ta = length (solve ta)
 
 main :: IO ()
 main = do
