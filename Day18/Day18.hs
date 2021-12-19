757798fc429834ae040d60cb6018a1cde7c1c7f3
jrp2014 <jrp2014@users.noreply.github.com>
2021-12-19 00:11:18 +0000

    More tests

--- Day18/Day18.hs
+++ Day18/Day18.hs
@@ -113,10 +113,19 @@ addDownR n (N m) = N (n + m)
 -- | Compute the /magnitude/ of an expression
 --
 -- >>> magnitude (head $ parse "[9,1]")
--- 29
---
+-- 29ß
 -- >>> magnitude (head $ parse "[[1,2],[[3,4],5]]")
 -- 143
+-- >>> magnitude (parseLine "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]")
+-- 1384
+-- >>> magnitude (parseLine "[[[[1,1],[2,2]],[3,3]],[4,4]]")
+-- 445
+-- >>> magnitude (parseLine "[[[[3,0],[5,3]],[4,4]],[5,5]]")
+-- 791
+-- >>> magnitude (parseLine "[[[[5,0],[7,4]],[5,5]],[6,6]]")
+-- 1137
+-- >>> magnitude (parseLine "[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]")
+-- 3488
 magnitude :: X -> Int
 magnitude (N x) = x
 magnitude (l :+ r) = 3 * magnitude l + 2 * magnitude r
