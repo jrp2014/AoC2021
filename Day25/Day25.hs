64e22c090423dc5bc0935bafbd3a071d54ea51b1
jrp2014 <jrp2014@users.noreply.github.com>
2021-12-26 17:04:29 +0000

    Day 25 tidying

--- Day25/Day25.hs
+++ Day25/Day25.hs
@@ -4,10 +4,7 @@ module Day25.Day25 where
 
 import Data.Map (Map)
 import qualified Data.Map.Strict as Map
-import Data.Foldable
 
-
--- | A snailfish expression
 data Coord = C !Int !Int -- y x
   deriving (Show, Eq, Ord)
 
@@ -27,74 +24,72 @@ evolution (x : xs) = x : evolution xs
 evolution [] = []
 
 step :: Int -> Int -> Grid -> Grid
-step ny nx = step1 ny nx 'v' below . step1 ny nx '>' right
-
--- | Increment y coordinate
-below :: Coord -> Coord
-below (C y x) = C (y + 1) x
-
--- | Increment x coordinate
-right :: Coord -> Coord
-right (C y x) = C y (x + 1)
-
-step1 :: Int -> Int -> Char -> (Coord -> Coord) -> Grid -> Grid
-step1 ny nx c f inp =
-  Map.fromList
-    [ (if v == c && Map.notMember k' inp then k' else k, v)
-      | (k, v) <- Map.toList inp,
-        let k' = fixup (f k)
-    ]
+step ny nx = step1 'v' below . step1 '>' right
   where
-    fixup (C y x) = C (y `mod` ny) (x `mod` nx)
+    -- Increment y coordinate
+    below :: Coord -> Coord
+    below (C y x) = C ((y + 1) `mod` ny) x
 
--- | Find the upper-left and lower-right coordinates that
--- inclusively contain all the coordinates in a list of
--- coordinates.
-boundingBox :: [Coord] -> Maybe (Coord, Coord)
-boundingBox t =
-  case t of
-    [] -> Nothing
-    C y x : cs -> go y x y x cs
-  where
-    go loy lox hiy hix [] = Just (C loy lox, C hiy hix)
-    go loy lox hiy hix (C y x : cs) = go (min loy y) (min lox x) (max hiy y) (max hix x) cs
+    -- Increment x coordinate
+    right :: Coord -> Coord
+    right (C y x) = C y ((x + 1) `mod` nx)
+
+    step1 :: Char -> (Coord -> Coord) -> Grid -> Grid
+    step1 c delta grid =
+      Map.fromList
+        [ (if v == c && Map.notMember k' grid then k' else k, v)
+          | (k, v) <- Map.toList grid,
+            let k' = delta k
+        ]
 
 -- | Render a minimal bounding box containing all the characters
--- at the given coordinates. Empty space filled with space characters.
+-- at the given coordinates. Empty space filled with dots.
 drawPicture :: Grid -> String
 drawPicture pixels =
   case boundingBox (Map.keys pixels) of
     Nothing -> ""
     Just (C miny minx, C maxy maxx) ->
       unlines [[Map.findWithDefault '.' (C y x) pixels | x <- [minx .. maxx]] | y <- [miny .. maxy]]
+  where
+    -- Find the upper-left and lower-right coordinates that
+    -- inclusively contain all the coordinates in a list of
+    -- coordinates.
+    boundingBox :: [Coord] -> Maybe (Coord, Coord)
+    boundingBox [] = Nothing
+    boundingBox (C y x : cs) = go y x y x cs
+      where
+        go :: Int -> Int -> Int -> Int -> [Coord] -> Maybe (Coord, Coord)
+        go loy lox hiy hix [] = Just (C loy lox, C hiy hix)
+        go loy lox hiy hix (C y x : cs) = go (min loy y) (min lox x) (max hiy y) (max hix x) cs
 
 part1 :: Grid -> Int
-part1 inp = length (evolution steps)
+part1 grid = length (evolution steps)
   where
-    keys = Map.keys inp
-    ny = 1 + maximum [y | C y _  <- keys ]
-    nx = 1 + maximum [x | C _ x  <- keys ]
-    inp' = Map.filter (`elem` ">v") inp
+    coords = Map.keys grid
+    ny = 1 + maximum [y | C y _ <- coords]
+    nx = 1 + maximum [x | C _ x <- coords]
+    grid' = Map.filter (`elem` ">v") grid
+
     steps :: [Grid]
-    steps = iterate (step ny nx) inp'
+    steps = iterate (step ny nx) grid'
 
 part2 :: Grid -> Int
-part2 inp = undefined
+part2 grid = undefined
 
 main :: IO ()
 main = do
   putStr "Test Part 1: "
-  tinput <- readFile "test.txt"
-  let ptinput = parse tinput
-  print $ part1 ptinput
+  tgridut <- readFile "test.txt"
+  let ptgridut = parse tgridut
+  print $ part1 ptgridut
 
   putStr "Part 1: "
-  input <- readFile "input.txt"
-  let pinput = parse input
-  print $ part1 pinput
+  gridut <- readFile "input.txt"
+  let pgridut = parse gridut
+  print $ part1 pgridut
 
   putStr "Test Part 2: "
-  print $ part2 ptinput
+  print $ part2 ptgridut
 
   putStr "Part 2: "
-  print $ part2 pinput
+  print $ part2 pgridut
